a91bbfc188d0a3d2918b97fa606b7467620d2416
jonschlinkert <jon.schlinkert@sellside.com>
2017-07-20 17:04:57 -0400

    use `is-plain-object`

--- test.js
+++ test.js
@@ -1,28 +1,28 @@
 /*!
  * is-extendable <https://github.com/jonschlinkert/is-extendable>
  *
- * Copyright (c) 2015 .
- * Licensed under the MIT license.
+ * Copyright (c) 2015-2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
 
-/* deps: mocha */
+require('mocha');
 var assert = require('assert');
 var isExtendable = require('./');
 
-describe('isExtendable', function () {
-  it('should return true when a value is an object:', function () {
+describe('isExtendable', function() {
+  it('should return true when a value is an object:', function() {
     assert(isExtendable({}));
     assert(isExtendable([]));
     assert(isExtendable(function() {}));
-    assert(isExtendable(new RegExp('foo')));
-    assert(isExtendable(/foo/));
-    assert(isExtendable(new Date()));
-    assert(isExtendable(new Error()));
   });
 
-  it('should return false when a value is not an object:', function () {
+  it('should return false when a value is not an object:', function() {
+    assert(!isExtendable(new RegExp('foo')));
+    assert(!isExtendable(/foo/));
+    assert(!isExtendable(new Date()));
+    assert(!isExtendable(new Error()));
     assert(!isExtendable('a'));
     assert(!isExtendable(5));
     assert(!isExtendable(null));
